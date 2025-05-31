83d3175edbe0d73e33e73a7b291a9c96aec913b9
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2025-05-31 17:07:37 -0400

    implemented rom patching. just a sprite edit for now

--- worlds/mmzero3/Options.py
+++ worlds/mmzero3/Options.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
-from Options import Choice, Toggle, PerGameCommonOptions, StartInventoryPool
+from Options import Choice, PerGameCommonOptions
 
 class Goal(Choice):
     """
+    Default: Beat the final boss with enough data disks collected to win.
     Vanilla: Beat the final boss to win.
     Disk Hunt: Collect all data disks to win.
     """
     display_name = "Goal"
-    option_vanilla = 0
-    option_disk_hunt = 1
+    option_default = 0
+    option_vanilla = 1
+    option_disk_hunt = 2
     default = 0
 
 @dataclass
