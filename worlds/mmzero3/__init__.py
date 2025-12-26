b596731d63301f992a9cfa6ad293c2e0deac86b3
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2025-12-26 02:23:06 -0500

    Added base for ROM token edits

--- worlds/mmzero3/__init__.py
+++ worlds/mmzero3/__init__.py
@@ -140,6 +140,7 @@ class MMZero3World(World):
     def generate_output(self, output_directory: str) -> None:
         patch = Rom.MMZero3ProcedurePatch(player=self.player, player_name=self.player_name)
         patch.write_file("mmz3-ap.bsdiff4", pkgutil.get_data(__name__, "mmz3-ap.bsdiff4"))
+        Rom.write_tokens(self, patch)
         out_file_name = self.multiworld.get_out_file_name_base(self.player)
         patch.write(os.path.join(output_directory, f"{out_file_name}{patch.patch_file_ending}"))
 
