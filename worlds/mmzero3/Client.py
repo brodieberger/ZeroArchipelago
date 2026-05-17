60a78719b10873fe72af345947bc9393548d8dbb
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2026-05-17 14:38:04 -0400

    fixed bug regarding e tanks

--- worlds/mmzero3/Client.py
+++ worlds/mmzero3/Client.py
@@ -183,14 +183,14 @@ class MMZero3Client(BizHawkClient):
                 if other_items_found == b'\x01':
                     await ctx.send_msgs([{
                             "cmd": "LocationChecks",
-                            "locations": [218]
+                            "locations": [221]
                         }])
 
                 # Subtank 2 (Forest of Anatre)
                 elif (other_items_found == b'\x02'):
                     await ctx.send_msgs([{
                             "cmd": "LocationChecks",
-                            "locations": [219]
+                            "locations": [222]
                         }])
 
                 await bizhawk.write(
@@ -426,9 +426,9 @@ class MMZero3Client(BizHawkClient):
         ])
 
         tank_writes = []
-        if tank_1 == b'\xFF' and 221 in received_item_ids:
+        if tank_1 == b'\xFF' and 218 in received_item_ids:
             tank_writes.append((SUBTANK_1_ADDR, [0], "Combined WRAM"))
-        if tank_2 == b'\xFF' and 222 in received_item_ids:
+        if tank_2 == b'\xFF' and 219 in received_item_ids:
             tank_writes.append((SUBTANK_2_ADDR, [0], "Combined WRAM"))
         if tank_writes:
             await bizhawk.write(ctx.bizhawk_ctx, tank_writes)
